cb052c36a073f7f9e3b0168c3421c835276cd9fa
PPAvlidis7 <ppavlidis-2019>
2019-06-30 23:18:17 +0300

    Added a script for average read time calculation. Added a function to create tables at excel with our execution time measurements at average_time.py script. Added a text file with our execution time measurements. Added a text file with our read time measurements (sequential and parallel)

--- average_times.py
+++ average_times.py
@@ -1,61 +1,82 @@
+import sys
+import xlsxwriter
+sys.path.append('../')
+
+
 def create_dicts():
     time_dict = {
         'CSR': {
             '0.005': {
                 '5000': 0, '7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
-                '17000': 0, '19000': 0, '21000': 0, '23000': 0},
+                '17000': 0, '19000': 0, '21000': 0, '23000': 0
+            },
             '0.01': {
                 '5000': 0, '7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
-                '17000': 0, '19000': 0, '21000': 0, '23000': 0},
+                '17000': 0, '19000': 0, '21000': 0, '23000': 0
+            },
             '0.015': {
                 '5000': 0, '7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
-                '17000': 0, '19000': 0, '21000': 0, '23000': 0},
+                '17000': 0, '19000': 0, '21000': 0, '23000': 0
+            },
             '0.02':  {
                 '5000': 0, '7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
-                '17000': 0, '19000': 0, '21000': 0, '23000': 0}
+                '17000': 0, '19000': 0, '21000': 0, '23000': 0
+            }
         },
         'CSC': {
             '0.005': {
                 '5000': 0, '7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
-                '17000': 0, '19000': 0, '21000': 0, '23000': 0},
+                '17000': 0, '19000': 0, '21000': 0, '23000': 0
+            },
             '0.01': {
                 '5000': 0, '7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
-                '17000': 0, '19000': 0, '21000': 0, '23000': 0},
+                '17000': 0, '19000': 0, '21000': 0, '23000': 0
+            },
             '0.015': {
                 '5000': 0, '7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
-                '17000': 0, '19000': 0, '21000': 0, '23000': 0},
+                '17000': 0, '19000': 0, '21000': 0, '23000': 0
+            },
             '0.02':  {
                 '5000': 0, '7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
-                '17000': 0, '19000': 0, '21000': 0, '23000': 0}
+                '17000': 0, '19000': 0, '21000': 0, '23000': 0
+            }
         },
         'COO': {
             '0.005': {
                 '5000': 0, '7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
-                '17000': 0, '19000': 0, '21000': 0, '23000': 0},
+                '17000': 0, '19000': 0, '21000': 0, '23000': 0}
+            ,
             '0.01': {
                 '5000': 0, '7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
-                '17000': 0, '19000': 0, '21000': 0, '23000': 0},
+                '17000': 0, '19000': 0, '21000': 0, '23000': 0
+            },
             '0.015': {
                 '5000': 0, '7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
-                '17000': 0, '19000': 0, '21000': 0, '23000': 0},
+                '17000': 0, '19000': 0, '21000': 0, '23000': 0
+            },
             '0.02':  {
                 '5000': 0, '7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
-                '17000': 0, '19000': 0, '21000': 0, '23000': 0}
+                '17000': 0, '19000': 0, '21000': 0, '23000': 0
+            }
         },
         'Diagonal': {
             '0.005': {
                 '5000': 0, '7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
-                '17000': 0, '19000': 0, '21000': 0, '23000': 0},
+                '17000': 0, '19000': 0, '21000': 0, '23000': 0
+            },
             '0.01': {
                 '5000': 0, '7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
-                '17000': 0, '19000': 0, '21000': 0, '23000': 0},
+                '17000': 0, '19000': 0, '21000': 0, '23000': 0
+            },
             '0.015': {
                 '5000': 0, '7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
-                '17000': 0, '19000': 0, '21000': 0, '23000': 0},
+                '17000': 0, '19000': 0, '21000': 0, '23000': 0
+            },
             '0.02':  {
                 '5000': 0, '7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
-                '17000': 0, '19000': 0, '21000': 0, '23000': 0}
-        },
+                '17000': 0, '19000': 0, '21000': 0, '23000': 0
+            }
+        }
     }
 
     return time_dict
@@ -63,7 +84,7 @@ def create_dicts():
 
 def read_exec_times():
     times = []
-    with open('./exec_times.txt', 'r') as f:
+    with open('execution_time.txt', 'r') as f:
         for lines in f:
             times.append(lines.split())
     return times
@@ -81,6 +102,7 @@ def calculate_avg(exec_time, dict_times, type):
                 dict_times[index][inner_index][inner_inner_index] /= 10
     print(dict_times)
     write_times(dict_times, type)
+    return dict_times
 
 
 def write_times(t_dict, type):
@@ -92,7 +114,50 @@ def write_times(t_dict, type):
                 f.write("%s\t%s\t%s\n" % (key, key_inner, value))
 
 
+def create_tables(exec_time_dicts):
+    workbook = xlsxwriter.Workbook('seq_execution_times_table.xlsx')
+    worksheet = workbook.add_worksheet()
+    caption = 'Sequential execution time'
+    # Set the columns widths.
+    worksheet.set_column('B:F', 12)
+    _format = workbook.add_format()
+    # Write the caption.
+    worksheet.write('B1', caption)
+
+    row_index = 3
+    densities = ['0.005', '0.01', '0.015', '0.02']
+
+    general_columns_values = [5000, 7000, 9000, 11000, 13000, 15000, 17000, 19000, 21000, 23000]
+    time_values = []
+    for density in densities:
+        # transform our data to the needed format
+        for key, inner_dict in exec_time_dicts.items():
+            temp_list = [key]
+            for key_inner, value in inner_dict[density].items():
+                temp_list.append(value)
+            time_values.append(temp_list)
+
+        table_columns = [{'header': str(density)}]
+        for value in general_columns_values:
+            table_columns.append(
+                {'header': str(value), 'format': _format}
+            )
+
+        # Options to use in the table.
+        options = {'data': time_values,
+                   'columns': table_columns}
+
+        row_to_show = 'B' + str(row_index) + ':L' + str((row_index+4))
+        row_index += 8
+        # Add a table to the worksheet.
+        worksheet.add_table(row_to_show, options)
+        time_values = []
+
+    workbook.close()
+
+
 if __name__ == "__main__":
     exec_time_dicts = create_dicts()
     exec_times_file = read_exec_times()
-    calculate_avg(exec_times_file, exec_time_dicts, type)
+    exec_time_dicts = calculate_avg(exec_times_file, exec_time_dicts, type)
+    create_tables(exec_time_dicts)
