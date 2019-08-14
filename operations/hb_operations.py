0d2a111ec38e2eba1c76c929f6b308fe7678d98a
pavlos <pavlidis@hotmail.com>
2019-08-14 23:29:48 +0300

    Finished addition and subtraction refactoring. Separated hb's operations

--- operations/hb_operations.py
+++ operations/hb_operations.py
@@ -25,6 +25,7 @@ def csr_addition_matrices_hb(matrix_1: list, matrix_2: list):
     else:
         br, ib, jb = csr(matrix_2)
     cr, ic, jc = addition_algorithm_csr(ar, ia, ja, br, ib, jb)
+
     return cr, ic, jc
 
 
@@ -38,4 +39,83 @@ def _csr_addition_matrices_hb(file_name_1: str, file_name_2: str):
     else:
         br, ib, jb = csr(matrix_2)
     cr, ic, jc = addition_algorithm_csr(ar, ia, ja, br, ib, jb)
+
+    return cr, ic, jc
+
+
+@singledispatch
+def csr_subtraction_matrices_hb(matrix_1: list, matrix_2: list):
+    ar, ia, ja = csr(matrix_1)
+    if matrix_1 == matrix_2:
+        br, ib, jb = ar, ia, ja
+    else:
+        br, ib, jb = csr(matrix_2)
+    cr, ic, jc = subtraction_algorithm_csr(ar, ia, ja, br, ib, jb)
+
+    return cr, ic, jc
+
+
+@csr_subtraction_matrices_hb.register
+def _csr_subtraction_matrices_hb(file_name_1: str, file_name_2: str):
+    matrix_1 = read_file(file_name=file_name_1, return_list=True)
+    matrix_2 = read_file(file_name=file_name_2, return_list=True)
+    ar, ia, ja = csr(matrix_1)
+    if matrix_1 == matrix_2:
+        br, ib, jb = ar, ia, ja
+    else:
+        br, ib, jb = csr(matrix_2)
+    cr, ic, jc = subtraction_algorithm_csr(ar, ia, ja, br, ib, jb)
+
+    return cr, ic, jc
+
+
+@singledispatch
+def csc_addition_matrices_hb(matrix_1: list, matrix_2: list):
+    ar, ia, ja = csc(matrix_1)
+    if matrix_1 == matrix_2:
+        br, ib, jb = ar, ia, ja
+    else:
+        br, ib, jb = csc(matrix_2)
+    cr, ic, jc = addition_algorithm_csc(ar, ia, ja, br, ib, jb)
+
+    return cr, ic, jc
+
+
+@csc_addition_matrices_hb.register
+def _csc_addition_matrices_hb(file_name_1: str, file_name_2: str):
+    matrix_1 = read_file(file_name=file_name_1, return_list=True)
+    matrix_2 = read_file(file_name=file_name_2, return_list=True)
+    ar, ia, ja = csc(matrix_1)
+    if matrix_1 == matrix_2:
+        br, ib, jb = ar, ia, ja
+    else:
+        br, ib, jb = csc(matrix_2)
+    cr, ic, jc = addition_algorithm_csc(ar, ia, ja, br, ib, jb)
+
+    return cr, ic, jc
+
+
+@singledispatch
+def csc_subtraction_matrices_hb(matrix_1: list, matrix_2: list):
+    ar, ia, ja = csc(matrix_1)
+    if matrix_1 == matrix_2:
+        br, ib, jb = ar, ia, ja
+    else:
+        br, ib, jb = csc(matrix_2)
+    cr, ic, jc = subtraction_algorithm_csc(ar, ia, ja, br, ib, jb)
+
+    return cr, ic, jc
+
+
+@csc_subtraction_matrices_hb.register
+def _csc_subtraction_matrices_hb(file_name_1: str, file_name_2: str):
+    matrix_1 = read_file(file_name=file_name_1, return_list=True)
+    matrix_2 = read_file(file_name=file_name_2, return_list=True)
+    ar, ia, ja = csc(matrix_1)
+    if matrix_1 == matrix_2:
+        br, ib, jb = ar, ia, ja
+    else:
+        br, ib, jb = csc(matrix_2)
+    cr, ic, jc = subtraction_algorithm_csc(ar, ia, ja, br, ib, jb)
+
     return cr, ic, jc
--- operations/temp_add_sub.py
+++ operations/temp_add_sub.py
@@ -20,7 +20,7 @@ def csr_addition_matrices(matrix_1: list, matrix_2: list):
     """
     :param matrix_1: list of lists
     :param matrix_2: list of lists
-    :return: the result of the addition of tow matrices stored in csr format
+    :return: the result of the addition of two matrices stored in csr format
     """
     ar, ia, ja = csr(matrix_1)
     if matrix_1 == matrix_2:
@@ -37,7 +37,7 @@ def _csr_addition_matrices(file_name_1: str, file_name_2: str, processes_number=
     :param file_name_2: string
     :param processes_number: int
     :param file_path: string
-    :return: the result of the addition of tow matrices stored in csr format
+    :return: the result of the addition of two matrices stored in csr format
     """
     ar, ia, ja = csr(file_name_1, processes_number, file_path)
     if file_name_2 == file_name_1:
@@ -61,7 +61,7 @@ def _csr_addition_matrices(matrix_size_row_1: int, matrix_size_col_1: int, matri
     :param file_id_2: int
     :param processes_number: int
     :param file_path: string
-    :return: the result of the addition of tow matrices stored in csr format
+    :return: the result of the addition of two matrices stored in csr format
     """
     ar, ia, ja = csr(matrix_size_row_1, matrix_size_col_1, density, file_id_1, processes_number, file_path)
     if matrix_size_row_1 == matrix_size_row_2 and matrix_size_col_1 == matrix_size_col_2 and file_id_1 == file_id_2:
@@ -76,8 +76,7 @@ def csr_subtraction_matrices(matrix_1: list, matrix_2: list):
     """
    :param matrix_1: list of lists
    :param matrix_2: list of lists
-   ----------------------
-   :return: the result of the subtraction of tow matrices stored in csr format
+   :return: the result of the subtraction of two matrices stored in csr format
    """
     ar, ia, ja = csr(matrix_1)
     if matrix_1 == matrix_2:
@@ -88,51 +87,42 @@ def csr_subtraction_matrices(matrix_1: list, matrix_2: list):
 
 
 @csr_subtraction_matrices.register
-def _csr_subtraction_matrices(file_name_1: str, file_name_2: str):
+def _csr_subtraction_matrices(file_name_1: str, file_name_2: str, processes_number=mp.cpu_count(), file_path='../'):
     """
     :param file_name_1: string
     :param file_name_2: string
-    ----------------------
-    :return: the result of the subtraction of tow matrices stored in csr format
+    :return: the result of the subtraction of two matrices stored in csr format
     """
-    ar, ia, ja = csr(file_name_1)
+    ar, ia, ja = csr(file_name_1, processes_number, file_path)
     if file_name_2 == file_name_1:
         br, ib, jb = ar, ia, ja
     else:
-        br, ib, jb = csr(file_name_2)
+        br, ib, jb = csr(file_name_2, processes_number, file_path)
     return subtraction_algorithm_csr(ar, ia, ja, br, ib, jb)
 
 
 @csr_subtraction_matrices.register
-def _csr_subtraction_matrices(matrix_size_row: int, matrix_size_col: int, density: float, file_id_1: int,
-                                  file_id_2: int):
+def _csr_subtraction_matrices(matrix_size_row_1: int, matrix_size_col_1: int, matrix_size_row_2: int,
+                              matrix_size_col_2: int, density: float, file_id_1: int, file_id_2: int,
+                              processes_number=mp.cpu_count(), file_path='../'):
     """
-    :param matrix_size_row: int
-    :param matrix_size_col: int
+    :param matrix_size_row_1: int
+    :param matrix_size_col_1: int
+    :param matrix_size_row_2: int
+    :param matrix_size_col_2: int
     :param density: float
     :param file_id_1: int
     :param file_id_2: int
-    ----------------------
-    :return: the result of the subtraction of tow matrices stored in csr format
+    :param processes_number: int
+    :param file_path: string
+    :return: the result of the subtraction of two matrices stored in csr format
     """
-    ar, ia, ja = csr(matrix_size_row, matrix_size_col, density, file_id_1)
-    br, ib, jb = csr(matrix_size_row, matrix_size_col, density, file_id_2)
+    ar, ia, ja = csr(matrix_size_row_1, matrix_size_col_1, density, file_id_1, processes_number, file_path)
+    if matrix_size_row_1 == matrix_size_row_2 and matrix_size_col_1 == matrix_size_col_2 and file_id_1 == file_id_2:
+        br, ib, jb = ar, ia, ja
+    else:
+        br, ib, jb = csr(matrix_size_row_2, matrix_size_col_2, density, file_id_2, processes_number, file_path)
     return subtraction_algorithm_csr(ar, ia, ja, br, ib, jb)
-
-
-# function for hb experiments
-@csr_subtraction_matrices.register
-def _csr_subtraction_matrices(is_hb: bool, matrix: list, file_name: str):
-    ar, ia, ja = csr(matrix)
-    br, ib, jb = ar, ia, ja
-    start_time = time.time()
-    cr, ic, jc = subtraction_algorithm_csr(ar, ia, ja, br, ib, jb)
-    total_time = time.time() - start_time
-    if not os.path.exists('../execution_results'):
-        os.makedirs('../execution_results')
-    with open(os.path.join('../execution_results', 'add_sub_hb_time.txt'), 'a') as f:
-        f.write('subtraction_csr\t%s\t%.5f\n' % (file_name, total_time))
-    return cr, ic, jc
 
 
 @singledispatch
@@ -140,8 +130,7 @@ def csc_addition_matrices(matrix_1: list, matrix_2: list):
     """
    :param matrix_1: list of lists
    :param matrix_2: list of lists
-   ----------------------
-   :return: the result of the addition of tow matrices stored in csc format
+   :return: the result of the addition of two matrices stored in csc format
    """
     ar, ia, ja = csc(matrix_1)
     if matrix_2 == matrix_1:
@@ -152,51 +141,42 @@ def csc_addition_matrices(matrix_1: list, matrix_2: list):
 
 
 @csc_addition_matrices.register
-def _csc_addition_matrices(file_name_1: str, file_name_2: str):
+def _csc_addition_matrices(file_name_1: str, file_name_2: str,  processes_number=mp.cpu_count(), file_path='../'):
     """
     :param file_name_1: string
     :param file_name_2: string
-    ----------------------
-    :return: the result of the addition of tow matrices stored in csc format
+    :param processes_number: int
+    :param file_path: string
+    :return: the result of the addition of two matrices stored in csc format
     """
-    ar, ia, ja = csc(file_name_1)
+    ar, ia, ja = csc(file_name_1, processes_number, file_path)
     if file_name_1 == file_name_2:
         br, ib, jb = ar, ia, ja
     else:
-        br, ib, jb = csc(file_name_2)
+        br, ib, jb = csc(file_name_2, processes_number, file_path)
     return addition_algorithm_csc(ar, ia, ja, br, ib, jb)
 
 
 @csc_addition_matrices.register
-def _csc_addition_matrices(matrix_size_row: int, matrix_size_col: int, density: float, file_id_1: int,
-                               file_id_2: int):
+def _csc_addition_matrices(matrix_size_row_1: int, matrix_size_col_1: int, matrix_size_row_2: int,
+                           matrix_size_col_2: int, density: float, file_id_1: int, file_id_2: int,
+                           processes_number=mp.cpu_count(), file_path='../'):
     """
-    :param matrix_size_row: int
-    :param matrix_size_col: int
+    :param matrix_size_row_1: int
+    :param matrix_size_col_1: int
+    :param matrix_size_row_2: int
+    :param matrix_size_col_2: int
     :param density: float
     :param file_id_1: int
     :param file_id_2: int
-    ----------------------
-    :return: the result of the addition of tow matrices stored in csc format
+    :return: the result of the addition of two matrices stored in csc format
     """
-    ar, ia, ja = csc(matrix_size_row, matrix_size_col, density, file_id_1)
-    br, ib, jb = csc(matrix_size_row, matrix_size_col, density, file_id_2)
+    ar, ia, ja = csc(matrix_size_row_1, matrix_size_col_1, density, file_id_1, processes_number, file_path)
+    if matrix_size_row_1 == matrix_size_row_2 and matrix_size_col_1 == matrix_size_col_2 and file_id_1 == file_id_2:
+        br, ib, jb = ar, ia, ja
+    else:
+        br, ib, jb = csc(matrix_size_row_2, matrix_size_col_2, density, file_id_2, processes_number, file_path)
     return addition_algorithm_csc(ar, ia, ja, br, ib, jb)
-
-
-# function for hb experiments
-@csc_addition_matrices.register
-def _csc_addition_matrices(is_hb: bool, matrix: list, file_name: str):
-    ar, ia, ja = csc(matrix)
-    br, ib, jb = ar, ia, ja
-    start_time = time.time()
-    cr, ic, jc = addition_algorithm_csc(ar, ia, ja, br, ib, jb)
-    total_time = time.time() - start_time
-    if not os.path.exists('../execution_results'):
-        os.makedirs('../execution_results')
-    with open(os.path.join('../execution_results', 'add_sub_hb_time.txt'), 'a') as f:
-        f.write('addition_csc\t%s\t%.5f\n' % (file_name, total_time))
-    return cr, ic, jc
 
 
 @singledispatch
@@ -204,7 +184,6 @@ def csc_subtraction_matrices(matrix_1: list, matrix_2: list):
     """
    :param matrix_1: list of lists
    :param matrix_2: list of lists
-   ----------------------
    :return: the result of the subtraction of tow matrices stored in csc format
    """
     ar, ia, ja = csc(matrix_1)
@@ -216,51 +195,42 @@ def csc_subtraction_matrices(matrix_1: list, matrix_2: list):
 
 
 @csc_subtraction_matrices.register
-def _csc_subtraction_matrices(file_name_1: str, file_name_2: str):
+def _csc_subtraction_matrices(file_name_1: str, file_name_2: str, processes_number=mp.cpu_count(), file_path='../'):
     """
     :param file_name_1: string
     :param file_name_2: string
-    ----------------------
+    :param processes_number: int
+    :param file_path: string
     :return: the result of the subtraction of tow matrices stored in csc format
     """
-    ar, ia, ja = csc(file_name_1)
+    ar, ia, ja = csc(file_name_1, processes_number, file_path)
     if file_name_2 == file_name_1:
         br, ib, jb = ar, ia, ja
     else:
-        br, ib, jb = csc(file_name_2)
+        br, ib, jb = csc(file_name_2, processes_number, file_path)
     return subtraction_algorithm_csc(ar, ia, ja, br, ib, jb)
 
 
 @csc_subtraction_matrices.register
-def _csc_subtraction_matrices(matrix_size_row: int, matrix_size_col: int, density: float, file_id_1: int,
-                                  file_id_2: int):
+def _csc_subtraction_matrices(matrix_size_row_1: int, matrix_size_col_1: int, matrix_size_row_2: int,
+                              matrix_size_col_2: int, density: float, file_id_1: int, file_id_2: int,
+                              processes_number=mp.cpu_count(), file_path='../'):
     """
-    :param matrix_size_row: int
-    :param matrix_size_col: int
+    :param matrix_size_row_1: int
+    :param matrix_size_col_1: int
+    :param matrix_size_row_2: int
+    :param matrix_size_col_2: int
     :param density: float
     :param file_id_1: int
     :param file_id_2: int
-    ----------------------
     :return: the result of the subtraction of tow matrices stored in csc format
     """
-    ar, ia, ja = csc(matrix_size_row, matrix_size_col, density, file_id_1)
-    br, ib, jb = csc(matrix_size_row, matrix_size_col, density, file_id_2)
+    ar, ia, ja = csc(matrix_size_row_1, matrix_size_col_1, density, file_id_1, processes_number, file_path)
+    if matrix_size_row_1 == matrix_size_row_2 and matrix_size_col_1 == matrix_size_col_2 and file_id_1 == file_id_2:
+        br, ib, jb = ar, ia, ja
+    else:
+        br, ib, jb = csc(matrix_size_row_2, matrix_size_col_2, density, file_id_2, processes_number, file_path)
     return subtraction_algorithm_csc(ar, ia, ja, br, ib, jb)
-
-
-# function for hb experiments
-@csc_subtraction_matrices.register
-def _csc_subtraction_matrices(is_hb: bool, matrix: list, file_name: str):
-    ar, ia, ja = csc(matrix)
-    br, ib, jb = ar, ia, ja
-    start_time = time.time()
-    cr, ic, jc = subtraction_algorithm_csc(ar, ia, ja, br, ib, jb)
-    total_time = time.time() - start_time
-    if not os.path.exists('../execution_results'):
-        os.makedirs('../execution_results')
-    with open(os.path.join('../execution_results', 'add_sub_hb_time.txt'), 'a') as f:
-        f.write('subtraction_csc\t%s\t%.5f\n' % (file_name, total_time))
-    return cr, ic, jc
 
 
 def main():
